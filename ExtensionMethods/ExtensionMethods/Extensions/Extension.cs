2409064c2671f1e26ba8e5049f4a2f992ab51d37
Hasan Can Kose <hasancannkosee@gmail.com>
2021-06-06 18:12:42 +0300

    Add extension methods

---
+++ ExtensionMethods/ExtensionMethods/Extensions/Extension.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExtensionMethods.Extensions
+{
+    public static class Extension
+    {
+
+        public static int AddFromOne(this int number)
+        {
+            int result = 0;
+            int i;
+
+            if (number > 0)
+            {
+                for (i = 0; i < number + 1; i++)
+                {
+                    result += i;
+                }
+            }
+            else
+            {
+                for (i = 0; i > number - 1; i--)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+
+        public static int AddEvenNumbersFromOne(this int number)
+        {
+            int result = 0;
+            int i;
+
+            if (number > 0)
+            {
+                for (i = 0; i < number + 1; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        result += i;
+                    }
+                }
+            }
+            else
+            {
+                for (i = 0; i > number - 1; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        result += i;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static int AddOddNumbersFromOne(this int number)
+        {
+            int result = 0;
+            int i;
+
+            if (number > 0)
+            {
+                for (i = 0; i < number + 1; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        result += i;
+                    }
+                }
+            }
+            else
+            {
+                for (i = 0; i > number - 1; i--)
+                {
+                    if (i % 2 != 0)
+                    {
+                        result += i;
+                    }
+                }
+            }
+            return result;
+        }
+        public static int ConvertToInteger(this string number)
+        {
+            return int.Parse(number, NumberStyles.AllowLeadingSign);
+        }
+    }
+}
--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExtensionMethods.Extensions;
 
 namespace ExtensionMethods
 {
@@ -6,7 +7,10 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string number = "-10";
+
+            int a = number.ConvertToInteger();
+            Console.WriteLine(a.AddOddNumbersFromOne());
         }
     }
 }
